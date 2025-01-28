47850ef3892df91c0ef2c35df131d8aba891fcb1
HyrumVonn <uberhimer@gmail.com>
2025-01-28 17:20:09 -0500

    Newton Raphson

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -12,6 +12,11 @@ globalPrecision = pow(10, -4)
 def f(x):
     return ( x + 4 ) * pow(x, 2) - 10
 
+#3x^2 + 8x
+#(3 * x + 8) x
+def derivativeF(x):
+    return (3 * x + 8) * x
+
 def ApproximationAlgorithm(a, b, precision):
     result = 0
 
@@ -42,9 +47,15 @@ def Bisection(a, b, precision):
         midY = f(result)
 
         if(ShareSigns(lY, midY)):
+            #if left and middle share a sign, then it must be
+            #between right and middle; set middle as new left, then
+            #repeat
             left = result
             error = numpy.abs(midY - rY)
         else :
+            #if left and middle do not share a sign, then it must
+            #be between left and middle; set middle as new right,
+            #then repeat
             right = result
             error = numpy.abs(midY - lY)
 
@@ -56,8 +67,20 @@ def FixedPointIteration(a, b, precision):
     return "Fixed point It"
 
 def NewtonRaphson(a, b, precision):
-    result = 0
-    return "Newton Raphson Method"
+    result = (a + b) / 2
+    iterations = 0
+
+    while(True):
+        iterations = iterations + 1
+        if((derivativeF(result) > 0) or (derivativeF(result) < 0)):
+            nextApprox = result - (f(result) / derivativeF(result))
+
+            if(numpy.abs(nextApprox - result) < precision):
+                return result
+            else:
+                result = nextApprox
+        else:
+            return f"Unsuccessful: f'(x) was 0 after {iterations} iterations\n"
 
 #Approximation
 print(f"{ApproximationAlgorithm(globalA, globalB, globalPrecision)}\n")
